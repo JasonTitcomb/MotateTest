321e740a789b29341c1022e13e5e50147c574438
Jason Titcomb <jason.titcomb@outlook.com>
2018-07-07 10:06:02 -0400

    Initial commit

---
+++ MotateTest/MotateTest.mk
@@ -0,0 +1,8 @@
+PROJECT  = MotateTest
+
+# Adjust this if you move the AwesomeProject folder:
+MOTATE_PATH ?= C:/GitHub/g2\Motate/MotateProject/motate
+
+NEEDS_PRINTF_FLOAT=0
+
+include $(MOTATE_PATH)/Motate.mk
---
+++ MotateTest/main.cpp
@@ -0,0 +1,23 @@
+/*
+ * MotateTest.cpp
+ *
+ * Created: 7/5/2018 7:25:37 PM
+ * Author : Jason
+ */ 
+
+
+#include "sam.h"
+#include "MotatePins.h"
+#include "MotateTimers.h"
+#include "MotateUART.h"
+
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
